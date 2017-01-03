25d5a3b911b1721750214b1f253316f85dfa403f
Naowak <yannisbendi@hotmail.Fr>
2017-01-03 10:00:22 +0100

    fin

--- projet Gestion Imprimantes/client/client.c
+++ projet Gestion Imprimantes/client/client.c
@@ -1,42 +1,238 @@
-/*
- ============================================================================
- Name        : client.c
- Author      : Amal Sayah
- ============================================================================
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdbool.h>
 
-#include "client.h"
+#include "../communication/communication.h"
+#include "../utils/utils.h"
 
-bool connexion(const char *serveur, const char *fichierDemande) {
-  int numCommunication;
-  int nbRecus;
-  char tampon[BUFSIZ];
-  int lgChemin;
-  int nbEnvoyes;
+#define NUMBER_IMPRESSION 50
+#define SIZE_BUFFER_RECEPTION 30
+#define SERVEUR_NAME "../serveur/serveur"
 
-  // se connecter au serveur
-  if ((numCommunication = demanderCommunication(serveur)) < 0) {
-    fprintf(stderr, "Client: erreur %s\n",
-            messageErreur((RetourCommunication)numCommunication));
-    return 1;
-  }
+int num_connexion;
 
-  // recevoir ensuite le contenu du fichier s'il existe
-  while ((nbRecus = recevoirOctets(numCommunication, tampon, BUFSIZ)) > 0)
-    write(1, tampon, nbRecus);
+Impression tab_impression[NUMBER_IMPRESSION];
+sem_t sem_tab_impression[NUMBER_IMPRESSION];
+sem_t places_tab_impression;
+bool est_disponible_tab_impression[NUMBER_IMPRESSION];
 
-  // clore la communication
-  cloreCommunication(numCommunication);
+int pipe_com_rec_imp_rec[2];
+int pipe_imp_rec_imp_env[2];
 
-  return 0;
+int pipe_retour[2];
+sem_t sem_pipe_retour;
+
+char imprimante_name[15];
+
+bool est_distante = false;
+
+
+void* reception(){
+	/** Receptionne les messages reçus par le serveur **/
+	while(1){
+		int result;
+		int id, n_commande;
+		char texte[30];
+		result = recevoirOctets(num_connexion, &id, sizeof(int));
+		if(result < 0){
+			perror("Erreur reception\n");
+			exit(1);
+		}
+		result = recevoirOctets(num_connexion, &n_commande, sizeof(int));
+		if(result < 0){
+			perror("Erreur reception\n");
+			exit(1);
+		}
+		result = recevoirOctets(num_connexion, texte, sizeof(char)*SIZE_BUFFER_RECEPTION);
+		if(result < 0){
+			perror("Erreur reception\n");
+			exit(1);
+		}
+
+		switch(n_commande){
+			case IMPRIMER :
+			{
+				write(pipe_com_rec_imp_rec[1], &id, sizeof(int));
+				write(pipe_com_rec_imp_rec[1], texte, sizeof(char)*strlen(texte));
+				break;
+			}
+			case ANNULER :
+				break;
+			case ETAT_IMPRIMANTE :
+				break;
+			case ETAT_IMPRESSION :
+				break;
+			default :
+				perror("Erreur commande inconnu\n");
+				break;
+		}
+	}
 }
 
-int main(int argc, char *argv[]) {
-  int ret;
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s nom_serveur ", argv[0]);
-    exit(2);
-  }
-  ret = connexion(argv[1]);
-  exit(ret);
+void* envoyer(){
+	/** Envois les messages de retour d'instruction au serveur **/
+	int id;
+	int retour;
+	int result;
+	while(1){
+		while(read(pipe_retour[0], &id, sizeof(int)) <= 0);
+		while(read(pipe_retour[0], &retour, sizeof(int)) <= 0);
+
+		result = envoyerOctets(num_connexion, &id, sizeof(int));
+		if(result < 0){
+			perror("Erreur Envois Octets");
+			exit(1);
+		}
+		result = envoyerOctets(num_connexion, &retour, sizeof(int));
+		if(result < 0){
+			perror("Erreur Envois Octets");
+			exit(1);
+		}
+	}
+}
+
+void* imprimer_reception(){
+	/** Ajoute à la liste d'impression les impressions demandées par le serveur **/
+	while(1){
+		Impression i;
+		i.texte = malloc(sizeof(char)*SIZE_BUFFER_RECEPTION);
+		while(read(pipe_com_rec_imp_rec[0], &i.id, sizeof(int)) <= 0);
+		while(read(pipe_com_rec_imp_rec[0], i.texte, sizeof(char)*SIZE_BUFFER_RECEPTION) <= 0);
+
+		sem_wait(&places_tab_impression);
+		int indice;
+		for(indice = 0; indice < NUMBER_IMPRESSION; ++indice)
+			if(est_disponible_tab_impression[indice]){
+				sem_wait(&sem_tab_impression[indice]);
+				est_disponible_tab_impression[indice] = false;
+				tab_impression[indice] = i;
+				sem_post(&sem_tab_impression[indice]);
+				break;
+			}
+
+		write(pipe_imp_rec_imp_env[1], &indice, sizeof(int));
+	}
+}
+
+void* imprimer_envois(){
+	/** Imprime et donne son retour (réussi ou non) à la fonction d'envois **/
+	while(1){
+		int indice, retour;
+		while(read(pipe_imp_rec_imp_env[0], &indice, sizeof(int)) <= 0);
+
+		sem_wait(&sem_tab_impression[indice]);
+
+		if(!est_disponible_tab_impression[indice]){
+			int file_lecture = open(tab_impression[indice].texte, O_RDONLY);
+			if(file_lecture == -1){
+				perror("Erreur ouverture file_lecture 1\n");
+				exit(1);
+			}
+			int file_ecriture;
+			if(est_distante){
+				file_ecriture = open(imprimante_name, O_WRONLY | O_APPEND | O_CREAT, 0666);
+				if(file_lecture == -1){
+					perror("Erreur ouverture file_ecriture 2\n");
+					exit(1);
+				}
+			}
+			else
+				file_ecriture = STDOUT_FILENO;
+
+			char c;
+			int result;
+			do{
+				result = read(file_lecture, &c, sizeof(char));
+				if(result > 0)
+					write(file_ecriture, &c, sizeof(char));
+			}while(result > 0);
+
+			est_disponible_tab_impression[indice] = true;
+			printf("Impression du fichier %s\n.", tab_impression[indice].texte);
+
+			close(file_ecriture);
+			close(file_lecture);
+
+			retour = 1;
+		}
+		else{
+			retour = 0;
+		}
+		sem_wait(&sem_pipe_retour);
+		write(pipe_retour[1], &tab_impression[indice].id, sizeof(int));
+		write(pipe_retour[1], &retour, sizeof(int));
+		sem_post(&sem_pipe_retour);
+		sem_post(&sem_tab_impression[indice]);
+		sem_post(&places_tab_impression);
+	}
+}
+
+int main(int argc, char* argv[]){
+	/** Initialise les variables et lance les threads **/
+	if(argc != 2 && argc != 3){
+		printf("Usage : <nom_imprimante> <optionnel : 1 si distante>\n");
+		exit(1);
+	}
+	char* nom_imprimante = argv[1];
+	if(argc == 3)
+		est_distante = true;
+
+	num_connexion = demanderCommunication(SERVEUR_NAME);
+	if(num_connexion < 0){
+		perror("Erreur Connexion Imprimante\n");
+		exit(1);
+	}
+	else{
+		if(est_distante)
+			printf("Imprimante Distante %s Connectée.\n", nom_imprimante);
+		else
+			printf("Imprimante %s Connectée.\n", nom_imprimante);
+	}
+
+	int result;
+	int i;
+	for(i = 0; i < NUMBER_IMPRESSION; ++i){
+		result = sem_init(&sem_tab_impression[i], 0, 1);
+		if(result == -1){
+			perror("Erreur Init Semaphore tab impression\n");
+			exit(1);
+		}
+	}
+	result = sem_init(&places_tab_impression, 0, NUMBER_IMPRESSION);
+	if(result == -1){
+		perror("Erreur Init Semaphore places_tab_impression\n");
+		exit(1);
+	}
+	result = sem_init(&sem_pipe_retour, 0, 1);
+	if(result == -1){
+		perror("Erreur Init Semaphore sem_pipe_retour\n");
+		exit(1);
+	}
+
+	pipe(pipe_com_rec_imp_rec);
+	pipe(pipe_imp_rec_imp_env);
+	pipe(pipe_retour);
+
+	for(i = 0; i < NUMBER_IMPRESSION; ++i)
+		est_disponible_tab_impression[i] = true;
+
+	sprintf(imprimante_name, "imprimante_%s", nom_imprimante);
+
+	pthread_t thread_reception, thread_imprimer_reception, thread_imprimer_envois;
+	pthread_t thread_envoyer;
+	pthread_create(&thread_reception, NULL, reception, NULL);
+	pthread_create(&thread_imprimer_reception, NULL, imprimer_reception, NULL);
+	pthread_create(&thread_imprimer_envois, NULL, imprimer_envois, NULL);
+	pthread_create(&thread_envoyer, NULL, envoyer, NULL);
+	pthread_join(thread_reception, NULL);
+	pthread_join(thread_imprimer_reception, NULL);
+	pthread_join(thread_imprimer_envois, NULL);
+	pthread_join(thread_envoyer, NULL);
+
+	return 0;
 }
--- projet Gestion Imprimantes/serveur/serveur.c
+++ projet Gestion Imprimantes/serveur/serveur.c
@@ -1,72 +1,199 @@
-/*
- ============================================================================
- Name        : client.c
- Author      : Amal Sayah
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 #include "../communication/communication.h"
+#include "../utils/utils.h"
 
-void serveur(const char *serveur)
-{
-  int numServeur, numCommunication;
-  char tampon[BUFSIZ];
-  int nbRecus;
+#define SIZE_BUFFER 30
+#define MAX_NUMBER_INSTRUCTONS 100
+#define NUMBER_IMPRIMANTES 20
+#define SERVEUR_NAME "serveur"
 
-  unlink(serveur);
+int pipe_envois[2];
+int pipe_retour[2];
+sem_t sem_pipe_retour;
 
-  // initialiser le serveur pour qu'il recoive les requetes
-   if ((numServeur = initialiserServeur(serveur)) < 0)
-  	  {
-	  fprintf(stderr, "Erreur initialiserServeur: %s: %s\n",
-			  serveur, messageErreur(numServeur));
-	  exit(1);
-  	  }
-  for (;;)
-	 {
-	 // accepter une connection
-	 if ((numCommunication = accepterCommunication(numServeur)) < 0)
-		 {
-		 fprintf(stderr, "Erreur accepterCommunication: %s: %s\n",
-				 serveur, messageErreur(numCommunication));
-	 	 exit(1);
-	   	 }
+Instruction tab_inst[MAX_NUMBER_INSTRUCTONS];
+int tab_imprimantes[NUMBER_IMPRIMANTES];
 
-	 // lire le nom du fichier à transferer
-	 if ((nbRecus = recevoirOctets(numCommunication, tampon, BUFSIZ)) > 0)
-		 {
-		 int inputFile;
-		 if ((inputFile = open(tampon, O_RDONLY)) != -1)
-			{
-			// envoyer le cntenu du fichier demande
-			int nbLus;
-		 	while ((nbLus = read(inputFile, tampon, BUFSIZ)) > 0)
-		 		{
-		 		if (envoyerOctets(numCommunication, tampon, nbLus) != nbLus)
-		 			break;
-		 		}
-		 	close(inputFile);
-			}
-		 }
+int num_serveur;
 
-	 // clore la communication
-	 cloreCommunication(numCommunication);
-	 }
+
+void* gestion_instruction(){
+	/** Lit l'entrée standard et redirige l'instruction vers la fonction d'envois **/
+	char buffer[SIZE_BUFFER];
+	int n_instruction = 0;
+	while(1){
+		Instruction i ;
+		int tmp_n_commande;
+		i.texte = malloc(sizeof(char)*SIZE_BUFFER);
+		scanf("%d %d %s",&i.n_printer,&tmp_n_commande, i.texte);
+		i.n_commande = (commande) tmp_n_commande;
+		i.id = n_instruction;
+		tab_inst[n_instruction++] = i;
+		
+		write(pipe_envois[1], &i.id, sizeof(int));
+		write(pipe_envois[1], &i.n_printer, sizeof(int));
+		write(pipe_envois[1], &i.n_commande, sizeof(int));
+		write(pipe_envois[1], i.texte, sizeof(char)*strlen(i.texte));
+		free(i.texte);
+		memset(buffer, 0, SIZE_BUFFER);
+	}
 }
 
-int main(int argc, char *argv[])
-{
-  int ret;
-  if (argc != 2)
-  	  {
-	  fprintf(stderr, "usage: %s nom_serveur\n", argv[0]);
-	  exit(2);
-  	  }
-  serveur(argv[1]);
-  exit(ret);
+void* communication_envois(){
+	/** Envois les instructions aux imprimantes correspondantes **/
+	int result;
+	Instruction i;
+	i.texte = malloc(sizeof(char)*SIZE_BUFFER);
+	while(1){
+		while(read(pipe_envois[0], &(i.id), sizeof(int)) <= 0 );
+		while(read(pipe_envois[0], &i.n_printer, sizeof(int)) <= 0);
+		while(read(pipe_envois[0], &i.n_commande, sizeof(int)) <= 0);
+		while(read(pipe_envois[0], i.texte, sizeof(char)*SIZE_BUFFER) <= 0);
+
+		result = envoyerOctets(i.n_printer, &i.id, sizeof(int));
+		if(result < 0){
+			perror("Erreur Envois Octets\n");
+			exit(1);
+		}
+		result = envoyerOctets(i.n_printer, &i.n_commande, sizeof(int));
+		if(result < 0){
+			perror("Erreur Envois Octets\n");
+			exit(1);
+		}
+		result = envoyerOctets(i.n_printer, i.texte, sizeof(char)*(SIZE_BUFFER - 4));
+		if(result < 0){
+			perror("Erreur Envois Octets\n");
+			exit(1);
+		}
+
+		memset(i.texte, 0, sizeof(char)*SIZE_BUFFER);
+		printf("La requête %d a été envoyée.\n", i.id);
+	}
+	free(i.texte);
+}
+
+
+void* communication_reception_retour(void* numero_imprimante){
+	/** Receptionne les retours d'instructions des imprimantes. 
+	Il existe un thread tournant sur cette fonction par imprimante **/
+	int nb_imp = *((int *) numero_imprimante);
+	int id, retour;
+	while(1){
+		int result;
+		result = recevoirOctets(nb_imp, &id, sizeof(int));
+		if(result < 0){
+			perror("Erreur reception\n");
+			exit(1);
+		}
+		result = recevoirOctets(nb_imp, &retour, sizeof(int));
+		if(result < 0){
+			perror("Erreur reception\n");
+			exit(1);
+		}
+
+		sem_wait(&sem_pipe_retour);
+		write(pipe_retour[1], &id, sizeof(int));
+		write(pipe_retour[1], &retour, sizeof(int));
+		sem_post(&sem_pipe_retour);
+	}
+}
+
+void* connexion(){
+	/** Gère les demandes de connexions de la part des imprimantes **/
+	int result;
+	int cmp = 0;
+	pthread_t tab_thread_retour[NUMBER_IMPRIMANTES];
+	while(1){
+		result = accepterCommunication(num_serveur);
+		if(result > 0){
+			printf("Nouvelle Imprimante Connectée au Serveur : %d\n", result);
+			tab_imprimantes[cmp] = result;
+			pthread_create(&tab_thread_retour[cmp++], NULL, communication_reception_retour, &result);
+		}
+	}
+}
+
+
+void* gestion_retour(){
+	/** Gère le retour des instructions données aux imprimantes **/
+	int id;
+	int retour;
+	while(1){
+		while(read(pipe_retour[0], &id, sizeof(int)) <= 0);
+		while(read(pipe_retour[0], &retour, sizeof(int)) <= 0);
+
+		switch(tab_inst[id].n_commande){
+			case IMPRIMER :
+				if(retour)
+					printf("L'impression ayant pour requête %d s'est terminée avec succès.\n", id);
+				else
+					printf("L'impression ayant pour requête %d n'existe plus.\n", id);
+				break;
+			case ANNULER :
+				break;
+			case ETAT_IMPRIMANTE :
+				break;
+			case ETAT_IMPRESSION :
+				break;
+			default :
+				perror("Erreur commande inconnu\n");
+				break;
+		}
+	}
+}
+
+
+
+int main(int argc, char* argv[]){
+	/** Initialise les variables et lancer les threads **/
+	int result;
+	result = pipe(pipe_envois);
+	if(result < 0 ){
+		perror("Error Pipe 1\n");
+		exit(1);
+	}
+	result = pipe(pipe_retour);
+	if(result < 0 ){
+		perror("Error Pipe 1\n");
+		exit(1);
+	}
+
+	unlink(SERVEUR_NAME);
+	num_serveur = initialiserServeur(SERVEUR_NAME);
+	if(num_serveur < 0){
+		perror("Error Init Server\n");
+		exit(1);
+	}
+
+	int i;
+	for(i = 0; i < NUMBER_IMPRIMANTES; ++i)
+		tab_imprimantes[i] = -1;
+
+	result = sem_init(&sem_pipe_retour, 0, 1);
+	if(result == -1){
+		perror("Erreur init sem_pipe_retour\n");
+		exit(1);
+	}
+
+
+	pthread_t thread_gestion_instruction, thread_com_envois, thread_connexion;
+	pthread_t thread_gestion_retour;
+	pthread_create(&thread_gestion_instruction, NULL, gestion_instruction, NULL);
+	pthread_create(&thread_com_envois, NULL, communication_envois, NULL);
+	pthread_create(&thread_connexion, NULL, connexion, NULL);
+	pthread_create(&thread_gestion_retour, NULL, gestion_retour, NULL);
+	pthread_join(thread_gestion_instruction, NULL);
+	pthread_join(thread_com_envois, NULL);
+	pthread_join(thread_connexion, NULL);
+	pthread_join(thread_gestion_retour, NULL);
+
+
+	return 0;
 }
